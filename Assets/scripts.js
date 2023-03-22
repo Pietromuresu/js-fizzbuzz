630238bdb35e12d4fb6bf368a2ac368ebd1a92cc
Pietromuresu <muresupietro66@gmail.com>
2023-03-22 18:13:01 +0100

    js add div with box class in DOM

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,20 +1,29 @@
 
+const fizz = 'Fizz';
+const buzz = 'Buzz';
+const limit = 100;
+for(i = 1; i <= limit; i++) {
 
-
-for(i = 1; i <= 100; i++) {
-
+  const container = document.querySelector('.container')
   let output = '';
 
   if (i % 3 === 0) {
-    output += 'Fizz';
+    output += fizz;
   }
   if (i % 5 === 0) {
-    output += 'Buzz';
+    output += buzz;
   }
    if (!output){
     output += i;
   }
   console.log(output);
+  const box = document.createElement('div');
+  box.classList.add('box')
+  box.append(output);
+  container.append(box)
+  
+
+
 }
   
 
---
+++ README.md
@@ -0,0 +1,13 @@
+# Js FizzBuzz
+
+- Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+---
+### Procedimento
+
+- stampare in console i numeri da 1 a 100
+- per i multipli di 3 rimpiazza il numero con Fizz (i % 3)
+- per i multipli di 5 rimpiazza il numero con Buzz (i % 5)
+- per i multipli sia di 3 che di 5 rimpiazza il numero con FizzBuzz
