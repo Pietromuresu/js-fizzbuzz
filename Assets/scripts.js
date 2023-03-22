cef6837f649cf47c5a7af5f403e19c3e77527625
Pietromuresu <muresupietro66@gmail.com>
2023-03-22 18:43:45 +0100

    display different bg-color for Fizz-Buzz

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,6 +1,8 @@
+const bgFizz = document.querySelector('.bgFizz');
+const bgBuzz = document.querySelector('.bgBuzz');
 
-const fizz = 'Fizz';
-const buzz = 'Buzz';
+const fizz = 'Fizz' 
+const buzz = 'Buzz' 
 const limit = 100;
 for(i = 1; i <= limit; i++) {
 
@@ -9,6 +11,7 @@ for(i = 1; i <= limit; i++) {
 
   if (i % 3 === 0) {
     output += fizz;
+    
   }
   if (i % 5 === 0) {
     output += buzz;
@@ -17,10 +20,27 @@ for(i = 1; i <= limit; i++) {
     output += i;
   }
   console.log(output);
+
   const box = document.createElement('div');
   box.classList.add('box')
   box.append(output);
   container.append(box)
+
+  if (output === fizz) {
+    box.classList.add('bgFizz')
+
+  }
+  if (output === buzz) {
+    box.classList.add('bgBuzz')
+  }
+
+  if (output === (fizz + buzz)) {
+    box.classList.add('bgBuzzFizz')
+   
+    
+
+  }
+ 
   
 
 
