610abfbee0a7f69d10d5098db7d59d5195bc6e89
Pietromuresu <muresupietro66@gmail.com>
2023-03-23 10:12:50 +0100

    Post- lesson corrections

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -3,23 +3,37 @@ const bgBuzz = document.querySelector('.bgBuzz');
 
 const fizz = 'Fizz' 
 const buzz = 'Buzz' 
+
+
 const limit = 100;
 for(i = 1; i <= limit; i++) {
 
   const container = document.querySelector('.container')
   let output = '';
 
-  if (i % 3 === 0) {
-    output += fizz;
+  // Soluzione meno bella perchè sovrascrive il vecchio output
+  // if (i % 3 === 0) {
+  //   output += fizz;
     
+  // }
+  // if (i % 5 === 0) {
+  //   output += buzz;
+  // }
+  //  if (!output){
+  //   output += i;
+  // }
+  // console.log(output);
+
+  // soluzione ottimale
+  if (!(i % 15)) {
+    output = fizz + buzz ;
+  }else if (!(i % 5)) {
+    output = buzz;
+  }else if (!(i % 3)) {
+    output = fizz;
+  }else {
+    output = i;
   }
-  if (i % 5 === 0) {
-    output += buzz;
-  }
-   if (!output){
-    output += i;
-  }
-  console.log(output);
 
   const box = document.createElement('div');
   box.classList.add('box')
